5c98239b6f75ea63fe7e570393be08b25fa46906
furnitureeigo <furnitureeigo@gmail.com>
2025-07-08 12:36:19 +0700

    Add files via upload

---
+++ src/utils/site.js
@@ -0,0 +1,9 @@
+export const url = 'https://furnitureeigo.workers.dev';
+export const nama = 'Bokep Indo Terbaru';
+export const desk = 'Video Bokep Indo Terbaru Bocil Ngentot Jilbab Smp Mama Viral Sma Cindo Tante';
+export const terbit = '2025-03-09T00:01:03Z';
+
+export const google = '6PhY1qnztta4gyFWqvqp0bM_dFfSux202SBKeT-VoWE';
+export const yandex = '91d24ddda6da8099';
+
+export const index = '8330f6aa-bbff-4a59-9622-f0772a8ea5dc';
